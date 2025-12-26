c8b734c1e02957595af11b8d18ebd48e4dac7939
tuanminh10133 <yamete123kudasai23@gmail.com>
2025-12-26 11:32:22 +0700

    Update app.py

--- python_ai/app.py
+++ python_ai/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
-import json
+from flask import Flask
 
 app = Flask(__name__)
 
-with open("data/korean.json", encoding="utf-8") as f:
-    KOREAN = json.load(f)
-
 @app.route("/")
 def home():
-    return render_template("index.html", data=KOREAN)
+    return "✅ App is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
